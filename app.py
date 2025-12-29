4fabe134dcc31561ca942e553025157b7c11d8ec
Sergio99-med <sergiof99@gmail.com>
2025-12-29 01:33:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ with tab1:
     archivo = st.file_uploader("Arrastra tu PDF aquí", type="pdf")
     
     # AVISO AZUL (MÁS VISIBLE)
-    st.info("ℹ️ Nota: Resultados NO numéricos (ej: orina) pueden no aparecer automáticamente. Digítalos manual si faltan.")
+    st.info("ℹ️ Nota: Resultados NO numéricos pueden no aparecer automáticamente. Digítalos manualmente si faltan.")
     
     if archivo:
         try:
@@ -125,7 +125,7 @@ with tab2:
     url = st.text_input("Pega el link del PDF aquí:")
     
     # AVISO AZUL
-    st.info("ℹ️ Nota: Si el link es de la Intranet, usa Ctrl+S y súbelo en la otra pestaña.")
+    st.info("Nota: Si el link es de la Intranet, usa Ctrl+S para guardarlo y súbelo en la otra pestaña.")
     
     if url:
         if st.button("Extraer desde Link"):
